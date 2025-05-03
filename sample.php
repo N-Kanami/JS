0c49ee13406525e2f5725457f957fb354f521511
N-Kanami <t4.kanami.26m@gmail.com>
2025-05-03 17:48:20 +0900

    PHPクラス・継承

--- sample.php
+++ sample.php
@@ -1,12 +1,59 @@
 <?php
-for($i =1;$i<20;$i++){
-    if($i == 14){
-        break;
+class Company
+{
+    protected $company_name;
+    protected $address;
+    protected $tel;
+    
+    public function displayCompany(){
+        echo '会社名:',$this->company_name;
+        echo "\n";
+        echo '住所:'.$this->address;
+        echo "\n";
+        echo '電話番号:',$this->tel;
+        echo "\n";
     }
-    if($i % 2 == 0){ 
-        continue;
+
+    public function setCompany($company_name,$address,$tel){
+        $this->company_name=$company_name;
+        $this->address=$address;
+        $this->tel=$tel;
     }
-    echo $i. "\n";
 }
-    echo "for_out";
+
+class Department extends Company
+{
+    private $department;
+    private $general_manager;
+
+    public function displayDepartment(){
+        echo '部署名:',$this->department;
+        echo "\n";
+        echo '部長:',$this->general_manager;
+        echo "\n"."\n";
+    }  
+
+    public function setDepartment($department,$general_manager){
+        $this->department=$department;
+        $this->general_manager=$general_manager;  
+    }
+}
+
+$sampleA=new Department();
+$sampleA->setCompany('サンプルA','市ヶ谷','080-XXXX-XXXX');
+$sampleA->setDepartment('人事','高田');
+
+$sampleB=new Department();
+$sampleB->setCompany('サンプルB','池袋','090-XXXX-XXXX');
+$sampleB->setDepartment('総務','田中');
+
+$sampleC=new Department();
+$sampleC->setCompany('サンプルC','新宿','070-XXXX-XXXX');
+$sampleC->setDepartment('営業','高橋');
+
+$samples=array($sampleA,$sampleB,$sampleC);
+foreach($samples as $samples){
+    echo $samples ->displayCompany();
+    echo $samples ->displayDepartment();
+};
 ?>
